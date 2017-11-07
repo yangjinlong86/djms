f435982d065008bf1391e8400d6156b84d86cc97
yangjinlong <yangjinlong86@gmail.com>
2017-11-07 22:08:43 +0800

    表单校验示例

---
+++ src/main/java/org/dj/bms/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.dj.bms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jason on 17/11/7.
+ */
+@Controller
+public class TestController {
+    @RequestMapping("/validate")
+    public String validate(){
+        return "validate";
+    }
+}
