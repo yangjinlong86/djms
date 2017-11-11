4237049eba1abe2acfd15d9b0cb244b9962b102d
davePu <pufangfei@163.com>
2017-11-11 15:52:18 +0800

    增加字典返回的显示项个数的默认

--- src/main/java/org/dj/bms/controller/DictController.java
+++ src/main/java/org/dj/bms/controller/DictController.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.dj.bms.model.Node;
 import org.dj.bms.utils.ECacheUtils;
 import org.dj.bms.utils.ResponseMsg;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,17 +21,30 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class DictController extends BaseController {
-
-	@RequestMapping("test/{pid}/{key}/{size}")
-	public Object testEache(@PathVariable("pid") String pid, @PathVariable("key") String key,
+	
+	@Value("${select.limit}")
+	private int limit = 10; 
+	/**
+	 * 
+	* @Title: testEache
+	* @Description: 通过关键字得到字典的值
+	* @param pid 字典类型
+	* @param key 关键字
+	* @param size 返回条数 默认为 10条
+	* @return Object    
+	* @throws
+	 */
+	@RequestMapping("dict/getValues/{pid}/{key}/{size}")
+	public Object dictEache(@PathVariable("pid") String pid,
+			@PathVariable("key") String key,
 			@PathVariable("size") int size) {
 		Map<String, Node> dict = ECacheUtils.getDictCache();
 		List<Node> nodes = new ArrayList<>();
-
+		int sum = size == 0? limit :size;
 		dict.forEach((k, v) -> {
 			if (StringUtils.startsWith(k, pid)) {
 				if (StringUtils.contains(k, key)) {
-					if (size == nodes.size()) {
+					if (sum == nodes.size()) {
 						return;
 					}
 					nodes.add(v);
